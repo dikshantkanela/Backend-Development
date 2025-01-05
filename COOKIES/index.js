828dac82d7b4516a8571608c170166b6c23f1189
dikshantkanela <dikshant7156@gmail.com>
2025-01-05 19:10:12 +0530

    Cookie parsing

--- COOKIES/index.js
+++ COOKIES/index.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser()); //cookie parsing middleware for express (req.cookies)
 
 app.get("/greet",(req,res)=>{
-    res.send("HELLO")
+    console.log(req.cookies);
+    const {name} = req.cookies; // cookies are independent of routes, once they are sent they will be sent on every request!
+    res.send(`Hello, ${name}`);
 })
 
 app.get("/setname",(req,res)=>{
+    res.cookie("name","Dikshant")
     res.cookie("car","BMW M340i");
     res.cookie("bike","RE GT 650");
-    res.clearCookie("name") // removes a cookie
+    // res.clearCookie("name") // removes a cookie
     res.cookie("friend","Aryan",{httpOnly:true,secure:true,sameSite:true,maxAge:3000})
     res.send("SENT A COOKIE!");
 })
